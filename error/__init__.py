d3cb918af393b303e5720661ca1f60a941d2040e
vanshika8687 <136159089+vanshika8687@users.noreply.github.com>
2023-06-12 18:44:48 +0530

    Update __init__.py

--- error/__init__.py
+++ error/__init__.py
@@ -6,6 +6,6 @@ import time
 openai_key = "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"
 api_id = "1125689"
 api_hash = "4772d1792ed194020a8fb06a91ffb8fa"
-bot_token = "6092990784:AAFrByXlLqyI7alkYJMdo2GhVRcrUzNLoNI"
+bot_token = "6092990784:AAFNvJyf_bQu7wbOUiDND403gjjxpiWcNH8"
 
 bot=TelegramClient("a", api_id, api_hash).start(bot_token=bot_token)
